908e8152292874c040811f7dde005aa4060259ac
JoseluGames <joselugamesofficial@gmail.com>
2017-08-20 13:14:34 +0200

    Shorten the code

--- src/main/java/com/jlgm/elemslimes/tileentity/TileEntityCorralMaster.java
+++ src/main/java/com/jlgm/elemslimes/tileentity/TileEntityCorralMaster.java
@@ -10,81 +10,49 @@ import net.minecraft.util.math.BlockPos;
 public class TileEntityCorralMaster extends TileEntity{
 	public void checkForCorral(EnumFacing facing) {
 		//Check for poles
+		int longDistance = 6;
+		int shortDistance = 3;
+		
+		if(facing.getAxisDirection() == EnumFacing.AxisDirection.NEGATIVE) {
+			longDistance *= -1;
+		}
+		
 		if(facing.getAxis() == EnumFacing.Axis.X) {
-			if(facing.getAxisDirection() == EnumFacing.AxisDirection.POSITIVE) {
-				ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() - 3, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() - 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() + 3, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() + 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 6, this.getPos().getY(), this.getPos().getZ() - 3, this.getPos().getX() + 6, this.getPos().getY() + 3, this.getPos().getZ() - 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 6, this.getPos().getY(), this.getPos().getZ() + 3, this.getPos().getX() + 6, this.getPos().getY() + 3, this.getPos().getZ() + 3)) {
-					listOfPoles.add(pos);
-				}
-				
-				for(BlockPos pos : listOfPoles) {
-					System.out.println(this.world.getBlockState(pos).getBlock());
-				}
-			}else if(facing.getAxisDirection() == EnumFacing.AxisDirection.NEGATIVE) {
-				ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() - 3, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() - 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() + 3, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() + 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 6, this.getPos().getY(), this.getPos().getZ() - 3, this.getPos().getX() - 6, this.getPos().getY() + 3, this.getPos().getZ() - 3)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 6, this.getPos().getY(), this.getPos().getZ() + 3, this.getPos().getX() - 6, this.getPos().getY() + 3, this.getPos().getZ() + 3)) {
-					listOfPoles.add(pos);
-				}
-				
-				for(BlockPos pos : listOfPoles) {
-					System.out.println(this.world.getBlockState(pos).getBlock());
-				}
+			ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() - shortDistance, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() - shortDistance)) {
+				listOfPoles.add(pos);
 			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() + shortDistance, this.getPos().getX(), this.getPos().getY() + 3, this.getPos().getZ() + shortDistance)) {
+				listOfPoles.add(pos);
+			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + longDistance, this.getPos().getY(), this.getPos().getZ() - shortDistance, this.getPos().getX() + longDistance, this.getPos().getY() + 3, this.getPos().getZ() - shortDistance)) {
+				listOfPoles.add(pos);
+			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + longDistance, this.getPos().getY(), this.getPos().getZ() + shortDistance, this.getPos().getX() + longDistance, this.getPos().getY() + 3, this.getPos().getZ() + shortDistance)) {
+				listOfPoles.add(pos);
+			}
+			
+			for(BlockPos pos : listOfPoles) {
+				System.out.println(this.world.getBlockState(pos).getBlock());
+			}
+			
 		} else if(facing.getAxis() == EnumFacing.Axis.Z) {
-			if(facing.getAxisDirection() == EnumFacing.AxisDirection.POSITIVE) {
-				ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() - 3, this.getPos().getY() + 3, this.getPos().getZ())) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() + 3, this.getPos().getY() + 3, this.getPos().getZ())) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ() + 6, this.getPos().getX() - 3, this.getPos().getY() + 3, this.getPos().getZ() + 6)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ() + 6, this.getPos().getX() + 3, this.getPos().getY() + 3, this.getPos().getZ() + 6)) {
-					listOfPoles.add(pos);
-				}
-				
-				for(BlockPos pos : listOfPoles) {
-					System.out.println(this.world.getBlockState(pos).getBlock());
-				}
-			}else if(facing.getAxisDirection() == EnumFacing.AxisDirection.NEGATIVE) {
-				ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() - 3, this.getPos().getY() + 3, this.getPos().getZ())) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() + 3, this.getPos().getY() + 3, this.getPos().getZ())) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ() - 6, this.getPos().getX() - 3, this.getPos().getY() + 3, this.getPos().getZ() - 6)) {
-					listOfPoles.add(pos);
-				}
-				for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ() - 6, this.getPos().getX() + 3, this.getPos().getY() + 3, this.getPos().getZ() - 6)) {
-					listOfPoles.add(pos);
-				}
-				
-				for(BlockPos pos : listOfPoles) {
-					System.out.println(this.world.getBlockState(pos).getBlock());
-				}
+			ArrayList<BlockPos> listOfPoles = new ArrayList<BlockPos>();
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - shortDistance, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() - shortDistance, this.getPos().getY() + 3, this.getPos().getZ())) {
+				listOfPoles.add(pos);
+			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + shortDistance, this.getPos().getY(), this.getPos().getZ(), this.getPos().getX() + shortDistance, this.getPos().getY() + 3, this.getPos().getZ())) {
+				listOfPoles.add(pos);
+			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() - shortDistance, this.getPos().getY(), this.getPos().getZ() + longDistance, this.getPos().getX() - shortDistance, this.getPos().getY() + 3, this.getPos().getZ() + longDistance)) {
+				listOfPoles.add(pos);
+			}
+			for(BlockPos pos : BlockPos.getAllInBox(this.getPos().getX() + shortDistance, this.getPos().getY(), this.getPos().getZ() + longDistance, this.getPos().getX() + shortDistance, this.getPos().getY() + 3, this.getPos().getZ() + longDistance)) {
+				listOfPoles.add(pos);
+			}
+			
+			for(BlockPos pos : listOfPoles) {
+				System.out.println(this.world.getBlockState(pos).getBlock());
 			}
 		}
 	}
